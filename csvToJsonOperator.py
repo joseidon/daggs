4a810f25dc0c6def534a43294f020818ceb2c052
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-05 18:06:53 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -32,7 +32,7 @@ class csvToJsonOperator(BaseOperator):
                 data.append(dfs)
         print("Print List:")
         #print(data)df.append([1,2,3,4,5,6,7,8,9,0,11,12])
-        df = pd.concat(data, axis=1)
+        df = pandas.concat(data, axis=1)
         df.append(data)#,  ignore_index = True
         print(df)
         df.to_csv("/home/airflow/final/final.csv")
