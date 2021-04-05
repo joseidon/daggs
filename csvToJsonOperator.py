65abe97c71d2fa59455febd7127b78eb2f637fc6
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-05 12:52:54 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -14,7 +14,7 @@ class csvToJsonOperator(BaseOperator):
 
     def execute(self, context):
         mypath = '/home/airflow/xkcd/'
-        df = dataframe
+        df = pandas.dataframe()
         latest_download = 1
         onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
         for f in onlyfiles:
