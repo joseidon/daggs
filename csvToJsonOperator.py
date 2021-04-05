ec2a02779a474ac7ddc54d3d9f9b4ed3ce787b1b
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-05 12:58:49 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -14,10 +14,10 @@ class csvToJsonOperator(BaseOperator):
 
     def execute(self, context):
         mypath = '/home/airflow/xkcd/'
-        df = pandas.dataframe()
+        df = pandas.DatafFrame()
         latest_download = 1
         onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
         for f in onlyfiles:
             with open('{}{}'.format(mypath,f)) as json_file:
-                pandas.read_json(json_file)
+                df.append(pandas.read_json(json_file))
         df.to_csv("{}/final/".format(mypath))
