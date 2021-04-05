ced0505a33d2941a344d8a69a0483f804792567f
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-05 17:04:35 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -24,8 +24,8 @@ class csvToJsonOperator(BaseOperator):
         for f in onlyfiles:
             with open('{}{}'.format(mypath,f)) as json_file:
                 rj = pandas.read_json(json_file, typ = 'series')
-                dfs = pandas.DataFrame(columns = ["month", "num", 'link', 'year', 'news', 'safe_title', 'transscript', 'alt', 'img', 'title', 'day'])
-                dfs = dfs.append(pandas.DataFrame.from_dict(rj))
+                #dfs = pandas.DataFrame(columns = ["month", "num", 'link', 'year', 'news', 'safe_title', 'transscript', 'alt', 'img', 'title', 'day'])
+                dfs = pandas.DataFrame.from_dict(rj)
                 print(dfs)
                 dfs = dfs.transpose()
                 print(dfs)
