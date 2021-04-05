5bcb61f28c171b5662d9fcbf2d3143fd451e62da
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-05 13:51:48 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -24,5 +24,6 @@ class csvToJsonOperator(BaseOperator):
             with open('{}{}'.format(mypath,f)) as json_file:
                 rj = pandas.read_json(json_file, typ = 'series')
                 dfs = pandas.DataFrame.from_dict(rj)
+                print(dfs)
                 df.append(dfs, ignore_index = True)
         df.to_csv("/home/airflow/final/final.csv")
