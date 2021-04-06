8c47b29ef0884876abc9210dea4d1a9d3605b9b0
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-06 18:50:35 +0200

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -21,16 +21,17 @@ args = {
  #wip
 hiveSQL_create_table_raw='''
 CREATE EXTERNAL TABLE IF NOT EXISTS raw_data(
-	tconst STRING,
-	title_type STRING,
-	primary_title STRING,
-	original_title STRING,
-	is_adult DECIMAL(1,0),
-	start_year DECIMAL(4,0),
-	end_year STRING,
-	runtime_minutes INT,
-	genres STRING
-) COMMENT 'IMDb Movies' PARTITIONED BY (partition_year int, partition_month int, partition_day int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_basics'
+	month INT,
+	num INT,
+	link STRING,
+	year INT,
+	news STRING,
+	safe_title STRING,
+	transcript STRING,
+	alt STRING,
+	title STRING,
+    day INT,
+) COMMENT 'raw DATA' PARTITIONED BY (year int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_basics'
 TBLPROPERTIES ('skip.header.line.count'='1');
 '''
 
