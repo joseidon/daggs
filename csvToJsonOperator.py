7ef8d18c079cdb9b07742a7f7e6cbb36eab5ea7c
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-08 13:19:27 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -29,10 +29,11 @@ class csvToJsonOperator(BaseOperator):
                 #print(dfs)
                 
                 dfs = dfs.transpose()
-                dfs = dfs.drop(labels = ["link","news"], axis=1,)
-                dfs = dfs.insert(9,"years",dfs["year"], True)
+                dfs = dfs.insert(11,"years",dfs["year"], True)
+                dfs = dfs.drop(labels = ["link","news","year"], axis=1,)
+                #dfs = dfs.insert(9,"years",dfs["year"], True)
                 #df['years'] = df['year']
-                df.drop(columns=["year"])
+                #df.drop(columns=["year"])
                 dfs = dfs.reset_index(drop=True)
                 dfs =dfs.replace(r'\n',' ', regex=True) 
                 print(dfs)
