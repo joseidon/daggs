0e5cfc1d4fce2537264975018b95ee6e17ffbd80
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-08 12:05:01 +0200

    Update csvToJsonOperator.py

--- csvToJsonOperator.py
+++ csvToJsonOperator.py
@@ -35,7 +35,7 @@ class csvToJsonOperator(BaseOperator):
                 data.append(dfs)
         print("Print List:")
         #print(data)df.append([1,2,3,4,5,6,7,8,9,0,11,12])
-        df = pandas.concat(data, axis=0)
+        df = pandas.concat(data, axis=0, ignore_index=true)
         #df.drop(labels = ["link","news"], axis=1,)
         #df.append(data)#,  ignore_index = True
         print(df)
