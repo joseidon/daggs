20cf3803a6e20f25f2479ec8bf8b0dc73796cf82
joseidon <61836330+joseidon@users.noreply.github.com>
2021-04-08 21:43:03 +0200

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -9,6 +9,7 @@ from airflow.operators.filesystem_operations import CreateDirectoryOperator
 from airflow.operators.filesystem_operations import ClearDirectoryOperator
 from airflow.operators.hive_operator import HiveOperator
 #from airflow.operators.mysql_operator import MOperator
+from airflow.operators.postgres_operator import PostgresOperator
 from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.hive_to_mysql import HiveToMySQL
